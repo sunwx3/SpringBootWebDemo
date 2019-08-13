9e2cc30b71838644fa738d8a8a130a2babab9be9
sunwx <1163271592@qq.com>
2019-08-13 18:13:00 +0800

    sql脚本

---
+++ spring-boot-cache/employee.sql
@@ -0,0 +1,3 @@
+INSERT INTO `employee` VALUES (2, '李四', 'lisi@163.com', 1, 1);
+INSERT INTO `employee` VALUES (3, '王五', 'wangwu@163.com', 1, 2);
+INSERT INTO `employee` VALUES (4, '张三', 'zhangsan@163.com', NULL, NULL);
