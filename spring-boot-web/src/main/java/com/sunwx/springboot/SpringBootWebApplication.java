b24474eb82cdfd4565fc8cad5b9817de36374bb2
sunwx3 <50604488+sunwx3@users.noreply.github.com>
2019-10-09 20:24:33 +0800

    swagger-ui使用
    sunwx

--- spring-boot-web/src/main/java/com/sunwx/springboot/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/com/sunwx/springboot/SpringBootWebApplication.java
@@ -3,9 +3,11 @@ package com.sunwx.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.sunwx.springboot.mapper")
+@ComponentScan("com.sunwx.springboot.config")
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
--- spring-boot-web/src/main/java/com/sunwx/springboot/controller/AdminController.java
+++ spring-boot-web/src/main/java/com/sunwx/springboot/controller/AdminController.java
@@ -4,6 +4,10 @@ import com.sunwx.springboot.entity.ums_admin;
 import com.sunwx.springboot.service.AdminService;
 import com.sunwx.springboot.utils.PageUtils;
 import com.sunwx.springboot.vo.ResEnv;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+@Api(value = "测试用的管理员模块", description = "测试用的管理员模块。。。。")
 @RestController
 public class AdminController {
     @Resource
     AdminService adminService;
     @RequestMapping("/selectOne")
+    @ApiOperation(value = "查询一个用户",httpMethod = "POST",
+            notes="nickName 用户姓名" +
+                    "	\n phone 用户手机号" +
+                    "	\n sessionId 会话id" +
+                    "	\n useType 登录类型（此处不用管）" +
+                    "	\n userName 用户名")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query",dataType="String",
+                    name="data",value="" +
+                    "	\n user_name 用户名"+
+                    "   \n passwd 密码"+
+                    "   \n validCode 验证码",
+                    required=true,
+                    defaultValue="{\"user_name\":\"\",\"passwd\":\"\",\"validCode\":\"\"}")
+    })
     public ResEnv<List<ums_admin>> login(){
         return PageUtils.toResEnv(adminService.selectOne());
     }
