74f78c4f48af6f26da394a3f577259822998c90f
sunwx <1163271592@qq.com>
2019-10-09 18:04:37 +0800

    MD5加密
    sunwx3

--- spring-boot-web/src/main/java/com/sunwx/springboot/controller/UserLoginController.java
+++ spring-boot-web/src/main/java/com/sunwx/springboot/controller/UserLoginController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.util.DigestUtils;
 import java.util.List;
 
 
@@ -19,9 +19,11 @@ public class UserLoginController extends BaseController {
     public ModelMap userLogin(User user){
         try {
             List<User> userList = userService.userLogin(user);
+            //String md5Pass = DigestUtils.md5DigestAsHex(user.getPassword().getBytes()); //MD5加密方式
             logger.info(String.valueOf(userList));
             if(userList.size()>0){
                 return getModelMap(StateParameter.SUCCESS, userList, "操作成功");
+
             }else{
                 return getModelMap(StateParameter.FAULT, userList.toString(), "用户名或密码不正确");
             }
